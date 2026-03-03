70626f3b58e0cd314930d576821b658d74375243
gvnjpn-cmyk <gvnjpn@gmail.com>
2026-03-04 04:10:12 +0700

    Update mafia.js

--- plugin/mafia.js
+++ plugin/mafia.js
@@ -3,10 +3,17 @@
 const mafiaGame = require('../Games/mafia');
 
 const handler = async (m, ctx) => {
-  return mafiaGame(m, ctx);
+  const { sock, args } = ctx;
+
+  return mafiaGame.handleMafia(
+    sock,
+    m,
+    args,
+    m.key.remoteJid
+  );
 };
 
-handler.command = ['mafia', 'mf', 'mfa'];
+handler.command = ['mafia', 'mfa', 'werewolf', 'ww'];
 handler.tags = ['game'];
 handler.help = ['mafia'];
 
